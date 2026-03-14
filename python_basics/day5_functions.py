05ee8478a1dc7bd0eab7672ace0920f915398ed5
KOwshik222 <kowshikreddybolla@gmail.com>
2026-03-14 23:12:14 +0530

    Day 5 functions practice

---
+++ python_basics/day5_functions.py
@@ -0,0 +1,37 @@
+def greet():
+    print("welcome")
+greet()
+greet()
+print("=============================================================================")
+def greet(name):
+    print("hii",name)
+greet("koushik")
+greet("a")
+print("======================================================================")
+def add(a,b):
+    print(a+b)
+add(2,3)
+add(2,5)
+print("=====================================================")
+def square(a):
+    print
+    (a*a)
+square(4)
+print("=====================================")
+def even_odd(a):
+    if a % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+even_odd(2)
+even_odd(3)
+print("==============================================")
+
+def max(a,b):
+    if a>b:
+        print("a is greater")
+    else :
+        print("b is greater")
+max(2,3)
+max(9,7)
+print("======================================================")
