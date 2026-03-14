5a9bf56ef50384f77a3252af6016c0a46a69be13
KOwshik222 <kowshikreddybolla@gmail.com>
2026-03-14 22:35:05 +0530

    day 4 loops

---
+++ python_basics/day4_loops.py
@@ -0,0 +1,34 @@
+for i in range(5):
+    print("hello")
+print("____________________________________________________________")
+for i in range(1,6):
+    print("hello")
+print("___________________________________________________________")
+for student in range(5):
+    print("student number:", student)
+print("___________________________________________________________")
+fruits=["a","b","c"]
+for fruit in fruits:
+    print(fruit)
+print("______________________________________________________________")
+for i in range(11):
+    print(i)
+print("_______________________________________________________")
+for i in range(2,21,2):
+    print(i)
+print("____________________________________________________________")
+for i in range(10,0,-1):
+    print(i)
+print("_____________________________________________________________")
+n = int(input("enter the number"))
+total = 0
+for i in range(1,n+1):
+    total = total+i
+print("sum:", total)
+print("________________________________________________________________")
+i = 0
+while(i<20):
+    print(i)
+    i = i+12
+
+
