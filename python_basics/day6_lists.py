1297694f1683041ca5de92040ff158a9678311b9
KOwshik222 <kowshikreddybolla@gmail.com>
2026-03-15 14:55:12 +0530

    Day 6 lists practice

---
+++ python_basics/day6_lists.py
@@ -0,0 +1,24 @@
+fruits = ["apple", "banana", "mango"]
+print(fruits)
+print(fruits[0])
+fruits[1] = "orange"
+print(fruits)
+fruits.append("banana")
+print(fruits)
+for f in fruits:
+    print(f)
+print("=============================================================")
+marks=[10,20,30,40]
+print(marks[1:3])
+print(marks[:3])
+print(marks[2:])
+print("========================================================")
+numbers = []
+for i in range(5):
+    numbers.append(i)
+print(numbers)
+print("=======================================================")
+squares = []
+for i in range(5):
+    squares.append(i*i)
+print(squares)
