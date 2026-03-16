cf3f115827145b23b06bd1858d191078266d143a
KOwshik222 <kowshikreddybolla@gmail.com>
2026-03-16 22:39:11 +0530

    Day 8 tuples and sets practice

---
+++ python_basics/day8_tuples.py
@@ -0,0 +1,16 @@
+#A tuple is like a list, but it cannot be changed after creation. and used when there is no change in data
+
+numbers = (10,20,30,40)
+print(numbers)
+print(numbers[0])
+print(numbers[1])
+
+#sets - stores uniques values only
+numbers = {1,2,2,3,4}
+print(numbers)
+#Adding Elements to a Set
+numbers = {1,2,3}
+
+numbers.add(4)
+
+print(numbers)
